5dfc9dfe75669ed7562e5db12ab4209dfd6e765a
Scott Sickles <scottsickles@gmail.com>
2023-10-03 17:16:44 -0400

    just take in the secrets and store them

--- atlas.tf
+++ atlas.tf
@@ -1,11 +1,42 @@
 provider "mongodbatlas" {
-  public_key  = data.aws_secretsmanager_secret_version.atlas_public_key.secret_string
-  private_key = data.aws_secretsmanager_secret_version.atlas_private_key.secret_string
+  public_key  = var.atlas_public_key
+  private_key = var.atlas_private_key
 }
 
-data "aws_secretsmanager_secret_version" "atlas_public_key" {
-  secret_id = var.atlas_public_key_secret_id
+resource "aws_secretsmanager_secret" "atlas_public_key" {
+  name_prefix = "${local.block_name}/atlas_public_key/"
+  tags        = local.tags
+  kms_key_id  = aws_kms_key.this.arn
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
-data "aws_secretsmanager_secret_version" "atlas_private_key" {
-  secret_id = var.atlas_private_key_secret_id
+
+resource "aws_secretsmanager_secret_version" "atlas_public_key" {
+  secret_id     = aws_secretsmanager_secret.atlas_public_key.id
+  secret_string = var.atlas_public_key
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_secretsmanager_secret" "atlas_private_key" {
+  name_prefix = "${local.block_name}/atlas_private_key/"
+  tags        = local.tags
+  kms_key_id  = aws_kms_key.this.arn
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_secretsmanager_secret_version" "atlas_private_key" {
+  secret_id     = aws_secretsmanager_secret.atlas_private_key.id
+  secret_string = var.atlas_private_key
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,10 +1,12 @@
-variable "atlas_public_key_secret_id" {
+variable "atlas_public_key" {
   type        = string
+  sensitive   = true
   description = "The public key used to authenticate to your MongoDB Atlas account"
 }
 
-variable "atlas_private_key_secret_id" {
+variable "atlas_private_key" {
   type        = string
+  sensitive   = true
   description = "The private key used to authenticate to your MongoDB Atlas account"
 }
 
