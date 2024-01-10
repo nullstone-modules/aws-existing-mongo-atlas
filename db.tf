2161ede587c3a6197c02aa62c0fb4cb846d82f17
Scott Sickles <scottsickles@gmail.com>
2024-01-10 12:11:30 -0500

    make the lookup of db_endpoint safe

--- db.tf
+++ db.tf
@@ -6,7 +6,7 @@ data "mongodbatlas_advanced_cluster" "this" {
 }
 
 locals {
-  db_endpoint = lookup(data.mongodbatlas_advanced_cluster.this.connection_strings[0].aws_private_link_srv, aws_vpc_endpoint.this.id)
+  db_endpoint = try(lookup(data.mongodbatlas_advanced_cluster.this.connection_strings[0].aws_private_link_srv, aws_vpc_endpoint.this.id), "")
   db_protocol = split("://", local.db_endpoint)[0]
   db_port     = local.db_protocol == "mongodb+srv" ? 27016 : 27017
 }
